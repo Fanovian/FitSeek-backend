63e21ccc8d37265b64628c7db03252e681f535e3
Fanovian <fanovian@outlook.com>
2025-05-18 21:42:29 +0800

    https

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
+const fs = require('fs');
+const https = require('https');
 const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+// 假设你的证书放在 certs 目录下
+const options = {
+  key: fs.readFileSync('/home/ubuntu/api.fanovian.cc.key'), // 私钥文件
+  cert: fs.readFileSync('/home/ubuntu/api.fanovian.cc_bundle.crt'), // 证书文件
+  // 如果有中间证书（chain），加上这个：
+  // ca: fs.readFileSync('./certs/intermediate.crt')
+};
+
+// 创建 HTTPS 服务器
+https.createServer(options, app).listen(PORT, () => {
+  console.log(`HTTPS server running on port ${PORT}`);
 });
